aa1c93d07a7b1a33babb57b3bc1d3da1a90fec1e
CSHTML5 <contact@cshtml5.com>
2018-10-30 17:31:21 +0100

    Add UriKind and Uri.OriginalString

---
+++ Bridge/System/UriKind.cs
@@ -0,0 +1,23 @@
+﻿namespace System
+{
+    /// <summary>
+    /// Defines the kinds of System.Uris
+    /// </summary>
+    public enum UriKind
+    {
+        /// <summary>
+        /// The kind of the Uri is indeterminate.
+        /// </summary>
+        RelativeOrAbsolute = 0,
+
+        /// <summary>
+        /// The Uri is an absolute Uri.
+        /// </summary>
+        Absolute = 1,
+
+        /// <summary>
+        /// The Uri is a relative Uri.
+        /// </summary>
+        Relative = 2,
+    }
+}
