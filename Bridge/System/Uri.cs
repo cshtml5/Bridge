014a9b1eb840ddf7b50953e6bc59c2c47623615c
Pierre Barjon <barjon.pierre@gmail.com>
2020-04-01 15:55:43 +0200

    Implement several Uri properties

--- Bridge/System/Uri.cs
+++ Bridge/System/Uri.cs
@@ -3,11 +3,15 @@
     /// <summary>
     /// Provides an object representation of a uniform resource identifier (URI) and easy access to the parts of the URI.
     /// </summary>
-    [Bridge.Convention(Member = Bridge.ConventionMember.Field | Bridge.ConventionMember.Method, Notation = Bridge.Notation.CamelCase)]
-    [Bridge.External]
+    [Bridge.Convention(Member = Bridge.ConventionMember.Method | Bridge.ConventionMember.Field, Notation = Bridge.Notation.CamelCase)]
     [Bridge.Reflectable]
+    [Bridge.External]
     public class Uri
     {
+        /// <summary>
+        /// Initializes a new instance of the Uri class with the specified URI.
+        /// </summary>
+        /// <param name="uriString"></param>
         public extern Uri(string uriString);
 
         /// <summary>
@@ -19,12 +23,6 @@
         /// <param name="uriKind">Specifies whether the URI string is a relative URI, absolute URI, or is indeterminate.</param>
         public extern Uri(string uriString, UriKind uriKind);
 
-        public extern string AbsoluteUri
-        {
-            [Bridge.Template("getAbsoluteUri()")]
-            get;
-        }
-
         /// <summary>
         /// Gets the original URI string that was passed to the System.Uri constructor.
         /// </summary>
@@ -33,6 +31,77 @@
             [Bridge.Template("getOriginalString()")]
             get;
         }
+
+        /// <summary>
+        /// Gets whether the Uri instance is absolute.
+        /// </summary>
+        public extern bool IsAbsoluteUri
+        {
+            [Bridge.Template("getIsAbsolute()")]
+            get;
+        }
+
+        /// <summary>
+        /// Gets the absolute URI.
+        /// </summary>
+        public extern string AbsoluteUri
+        {
+            [Bridge.Template("getAbsoluteUri()")]
+            get;
+        }
+
+        /// <summary>
+        /// Gets the scheme name for this URI.
+        /// </summary>
+        public extern string Scheme
+        {
+            [Bridge.Template("getProtocol()")]
+            get;
+        }
+
+        /// <summary>
+        /// Gets the host component of this instance.
+        /// </summary>
+        public extern string Host
+        {
+            [Bridge.Template("getHostName()")]
+            get;
+        }
+
+        /// <summary>
+        /// Gets any query information included in the specified URI.
+        /// </summary>
+        public extern string Query
+        {
+            [Bridge.Template("getSearch()")]
+            get;
+        }
+
+        /// <summary>
+        /// Gets the port number of this URI.
+        /// </summary>
+        public extern int Port
+        {
+            [Bridge.Template("getPort()")]
+            get;
+        }
+
+        /// <summary>
+        /// Gets the escaped URI fragment.
+        /// </summary>
+        public extern string Fragment
+        {
+            [Bridge.Template("getHash()")]
+            get;
+        }
+
+        /// <summary>
+        /// Converts a string to its escaped representation.
+        /// </summary>
+        /// <param name="data">The string to escape.</param>
+        /// <returns>A System.String that contains the escaped representation of stringToEscape.</returns>
+        [Bridge.Template("encodeURIComponent({data})")]
+        public static extern string EscapeDataString(string data);
 
         [Bridge.Template("System.Uri.equals({uri1}, {uri2})")]
         public static extern bool operator ==(Uri uri1, Uri uri2);
